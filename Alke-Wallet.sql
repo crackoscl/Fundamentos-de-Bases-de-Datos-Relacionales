7fa5015291b9fa5b150a7ef9ff040ca7a48e45fb
Gilbert Lagos <gilbert.ata@gmail.com>
2024-03-21 23:19:38 -0300

    se corrigen errores en las consultas

--- Alke-Wallet.sql
+++ Alke-Wallet.sql
@@ -30,48 +30,26 @@ creationDate timestamp
 );
 
 INSERT INTO moneda (currency_name, currency_symbol, creationDate) 
-VALUES ('Rublo', '₽', CURRENT_TIMESTAMP);
-INSERT INTO moneda (currency_name, currency_symbol, creationDate) 
-VALUES ('Won', '₩', CURRENT_TIMESTAMP);
-INSERT INTO moneda (currency_name, currency_symbol, creationDate) 
-VALUES ('Yen japonés', '¥', CURRENT_TIMESTAMP);
-INSERT INTO moneda (currency_name, currency_symbol, creationDate) 
-VALUES ('Euro', '€', CURRENT_TIMESTAMP);
-INSERT INTO moneda (currency_name, currency_symbol, creationDate) 
-VALUES ('Dólar canadiense', 'C$', CURRENT_TIMESTAMP);
-INSERT INTO moneda (currency_name, currency_symbol, creationDate) 
-VALUES ('Dólar estadounidense', 'US$', CURRENT_TIMESTAMP);
+VALUES ('Rublo', '₽', CURRENT_TIMESTAMP),
+('Won', '₩', CURRENT_TIMESTAMP),
+('Yen japonés', '¥', CURRENT_TIMESTAMP),
+('Euro', '€', CURRENT_TIMESTAMP),
+('Dólar canadiense', 'C$', CURRENT_TIMESTAMP),
+('Dólar estadounidense', 'US$', CURRENT_TIMESTAMP);
 
 INSERT INTO usuario (nombre, correo_electronico,contrasegna,saldo,fecha_creacion) 
-VALUES ('Clarivel','clariv@hotmail.com','12345678', 100.000, CURRENT_TIMESTAMP);
-
-INSERT INTO usuario (nombre, correo_electronico,contrasegna,saldo,fecha_creacion) 
-VALUES ('Juancito','juanci@hotmail.com','12345678', 250.000, CURRENT_TIMESTAMP);
-
-INSERT INTO usuario (nombre, correo_electronico,contrasegna,saldo,fecha_creacion) 
-VALUES ('Luis','LuisA@hotmail.com','12345678', 250.000, CURRENT_TIMESTAMP);
-
-INSERT INTO usuario (nombre, correo_electronico,contrasegna,saldo,fecha_creacion) 
-VALUES ('Dante','DanteP@hotmail.com','12345678', 500.000, CURRENT_TIMESTAMP);
-
-INSERT INTO usuario (nombre, correo_electronico,contrasegna,saldo,fecha_creacion) 
-VALUES ('Francisco','Francis@hotmail.com','12345678', 500.000, CURRENT_TIMESTAMP);
-
+VALUES ('Clarivel','clariv@hotmail.com','12345678', 100.000, CURRENT_TIMESTAMP),
+('Luis','LuisA@hotmail.com','12345678', 250.000, CURRENT_TIMESTAMP),
+('Dante','DanteP@hotmail.com','12345678', 500.000, CURRENT_TIMESTAMP),
+('Francisco','Francis@hotmail.com','12345678', 500.000, CURRENT_TIMESTAMP),
+('Juancito','juanci@hotmail.com','12345678', 250.000, CURRENT_TIMESTAMP);
 
 INSERT INTO transaccion(sender_user_id, receiver_user_id, moneda_id, importe, transaction_date) 
-VALUES (1,2,3,500.000,CURRENT_TIMESTAMP);
-
-INSERT INTO transaccion(sender_user_id, receiver_user_id, moneda_id, importe, transaction_date) 
-VALUES (1,4,1,600.000,CURRENT_TIMESTAMP);
-
-INSERT INTO transaccion(sender_user_id, receiver_user_id, moneda_id, importe, transaction_date) 
-VALUES (2,4,2,650.000,CURRENT_TIMESTAMP);
-
-INSERT INTO transaccion(sender_user_id, receiver_user_id, moneda_id, importe, transaction_date) 
-VALUES (4,1,2,250.000,CURRENT_TIMESTAMP);
-
-INSERT INTO transaccion(sender_user_id, receiver_user_id, moneda_id, importe, transaction_date)
-VALUES (5,2,5,450.000,CURRENT_TIMESTAMP);
+VALUES (1,2,3,500.000,CURRENT_TIMESTAMP), 
+(1,4,1,600.000,CURRENT_TIMESTAMP),
+(2,4,2,650.000,CURRENT_TIMESTAMP),
+(4,1,2,250.000,CURRENT_TIMESTAMP),
+(5,2,5,450.000,CURRENT_TIMESTAMP);
 
 
 -- Consulta para obtener el nombre de la moneda elegida por un usuario específico
@@ -90,23 +68,23 @@ INNER JOIN transaccion ON transaccion.moneda_id = moneda.currency_id
 INNER JOIN usuario ON  transaccion.sender_user_id =  usuario.user_id
 WHERE user_id = 4;
 
-
 -- Consulta para obtener todas las transacciones registradas
-SELECT transaction_id, sender.nombre AS sender_user, receiver.nombre AS receiver_user,currency_symbol, importe, transaction_date
+SELECT transaction_id, sender.nombre AS sender_user, receiver.nombre AS receiver_user,moneda.currency_symbol, importe, transaction_date
 FROM transaccion
-INNER JOIN usuario AS sender ON sender.user_id = transaccion.sender_user_id
-INNER JOIN usuario AS receiver ON receiver.user_id = transaccion.receiver_user_id
-INNER JOIN moneda ON currency_id = moneda.currency_id
+INNER JOIN usuario AS sender ON  transaccion.sender_user_id= sender.user_id 
+INNER JOIN usuario AS receiver ON  transaccion.receiver_user_id = receiver.user_id 
+INNER JOIN moneda ON transaccion.moneda_id = moneda.currency_id
 ORDER BY transaction_id ASC;
 
 -- Consulta para obtener todas las transacciones realizadas por un usuario específico
 SELECT transaction_id, sender.nombre AS sender_user, receiver.nombre AS receiver_user,currency_symbol, importe, transaction_date
 FROM transaccion
-INNER JOIN usuario AS sender ON sender.user_id = transaccion.sender_user_id
-INNER JOIN usuario AS receiver ON receiver.user_id = transaccion.receiver_user_id
-INNER JOIN moneda ON currency_id = moneda.currency_id
+INNER JOIN usuario AS sender ON  transaccion.sender_user_id= sender.user_id 
+INNER JOIN usuario AS receiver ON  transaccion.receiver_user_id = receiver.user_id 
+INNER JOIN moneda ON transaccion.moneda_id = moneda.currency_id
 WHERE sender_user_id = 1
 ORDER BY transaction_id ASC;
+
 
 -- Sentencia DML para modificar el campo correo electrónico de un usuario específico
 UPDATE usuario SET correo_electronico = 'vamosClari@hotmail.com'
@@ -119,4 +97,3 @@ WHERE transaction_id = 2;
 SELECT * FROM transaccion;
 SELECT * FROM moneda;
 SELECT * FROM usuario;
-
